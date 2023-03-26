e8857fee271db4145158bccd6e614c09679c01b5
paulmaiyanideveloper <paulmaiyanideveloper@gmail.com>
2023-03-26 15:36:53 +0300

    restricted wallet account to 12

--- accounts/models.py
+++ accounts/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 class UserWallet(models.Model):
     # one user can have only one wallet
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    account_number = models.CharField(max_length=15, unique=True, null=True, blank=True)
+    account_number = models.CharField(max_length=12, unique=True, null=True, blank=True)
     # start balance with default of 0
     balance = models.DecimalField(default=0.00, decimal_places=2, max_digits=10)
 
